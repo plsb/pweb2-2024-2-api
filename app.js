1208af78456b6264e549c2476b3a8622a34aa7c3
pelusb <peluissaraiva@gmail.com>
2025-03-09 16:15:01 -0300

    Add settings CORS

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ import authRoutes from './routes/authRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import saleRoutes from './routes/saleRoutes.js';
+import cors from 'cors';
 
 const app = express();
+
+// Configurar CORS
+app.use(cors({
+    origin: "http://localhost:3001", // Permite apenas essa origem
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true // Se estiver usando autenticação com cookies
+}));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
