1b6db58d6a8d6c0633e387fa88379e0f067eb390
pelusb <peluissaraiva@gmail.com>
2025-03-14 18:59:14 -0300

    updating categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -13,11 +13,11 @@ import { authenticateToken, authorizeRole } from '../middleware/authMiddleware.j
 const router = express.Router();
 
 // Rotas protegidas para categorias
-router.post('/categories', authenticateToken, authorizeRole('admin'), createCategory);
+router.post('/categories', authenticateToken, createCategory);
 router.get('/categories', authenticateToken, getAllCategories);
 router.get('/categories/search', authenticateToken, getCategoryByName);
 router.get('/categories/:id', authenticateToken, getCategoryById);
-router.put('/categories/:id', authenticateToken, authorizeRole('admin'), updateCategory);
-router.delete('/categories/:id', authenticateToken, authorizeRole('admin'), deleteCategory);
+router.put('/categories/:id', authenticateToken, updateCategory);
+router.delete('/categories/:id', authenticateToken, deleteCategory);
 
 export default router;
